73580f3117927aa0a908d29d8271200d50cd351b
Manfred Alonso <alonsomanfred7@gmail.com>
2022-06-12 20:21:37 +0200

    Repetições

---
+++ Aula16/ambiente.js
@@ -0,0 +1,26 @@
+var c=1
+/*while (c<=8){
+    console.log(`passo ${c}`)
+    c++
+}*/
+
+do{
+    console.log(`passo${c}`)
+    c++
+}while(c<=8)
+
+
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
