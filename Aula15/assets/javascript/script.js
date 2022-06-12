174f2b81e482134748c75088b9c67b4faa71dff7
Manfred Alonso <alonsomanfred7@gmail.com>
2022-06-12 20:21:28 +0200

    Att-Aula15

--- Aula15/assets/javascript/script.js
+++ Aula15/assets/javascript/script.js
@@ -8,11 +8,11 @@ function verificar(){
     }else{
         var fsex=document.getElementsByName(`radsex`)
         var idade = ano - Number(fano.value)
-        var gênero=``
+        var genero=``
         var img= document.createElement('img')
         img.setAttribute('id','foto')
         if(fsex[0].checked){
-            gênero=`Homem`
+            genero=`Homem`
             if(idade>=0&& idade <10){
                 //criança
                 img.setAttribute('src','bebemasculino.jpg')
@@ -28,7 +28,7 @@ function verificar(){
             }
             
         }else if(fsex[1].checked){
-            gênero=`Mulher`
+            genero=`Mulher`
             if(idade>=0&& idade <10){
                 //criança
                 img.setAttribute('src','bebefeminino.jpg')
@@ -44,8 +44,9 @@ function verificar(){
             }
         }
         res.style.textAlign='center'
+        console.log(res)
         img.style.width='250px'
-        res.innerHTML=`Detectamos ${gênero} com ${idade} anos.`
+        res.innerHTML=`Detectamos ${genero} com ${idade} anos.`
         res.appendChild(img)
     }
 }
